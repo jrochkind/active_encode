74c4782526e820a79da524393721eefbd870fe28
Jonathan Rochkind <jonathan@dnil.net>
2022-04-19 15:16:05 -0400

    MediaConvert adapter has direct_output_lookup config
    To calculate output directly from the MediaConvert Job object, without requiring the CloudWatch logs.
    
    All the info we were returning is there EXCEPT the actual output URLs. Sufficient info is there to calculate/predict the output URLs though -- I have done that for HLS. I believe it should be do-able for other formats too, but I am not familiar with them to figure out what they should be, so for NOW the URL calculation is only there for HLS. (If it's important, I could try to familiarize myself with the others and how the adapter currently handles them and try to match them without CloudWatch too? Not sure what other outputs are in any actual use at present, if any?)
    
    This logic makes some assumptions, like that hteres only one input file and only one output group -- but I _think_ such assumptions were already being made, and the adapter at current is incapable of violating them. It does potentially introduce some additional fragility though.

--- spec/integration/media_convert_adapter_spec.rb
+++ spec/integration/media_convert_adapter_spec.rb
@@ -152,6 +152,8 @@ describe ActiveEncode::EngineAdapters::MediaConvertAdapter do
 
   describe "output" do
     it "contains all expected outputs" do
+
+
       completed_output.each do |expected_output|
         found_output = completed_job.output.find { |output| output.id == expected_output[:id] }
         expected_output.each_pair do |key, value|
@@ -174,6 +176,28 @@ describe ActiveEncode::EngineAdapters::MediaConvertAdapter do
         expect(error.encode).to be_a(ActiveEncode::Base)
         expect(error.encode.state).to eq(:completed)
       end
+    end
+  end
+
+  describe "direct_output_lookup" do
+    before do
+      ActiveEncode::Base.engine_adapter.direct_output_lookup = true
+    end
+
+    it "contains all expected outputs" do
+      completed_output.each do |expected_output|
+        found_output = completed_job.output.find { |output| output.id == expected_output[:id] }
+        expected_output.each_pair do |key, value|
+          expect(found_output.send(key)).to eq(value)
+        end
+      end
+    end
+
+    it "does not make cloudwatch queries" do
+      expect(cloudwatch_logs).not_to receive(:start_query)
+      expect(cloudwatch_logs).not_to receive(:get_query_results)
+
+      completed_job
     end
   end
 end
